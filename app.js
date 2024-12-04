1221ffc62864efbd18120c2bcbb43594c7ca1729
afgmaker <associationgamemaker@gmail.com>
2024-12-04 22:46:20 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+var oneYMember = document.getElementById("1y-membership");
+var threeYMember = document.getElementById("3y-membership");
+var honorMember = document.getElementById("honor-membership");
+
+oneYMember.addEventListener("click", function(){
+	window.open("https://buy.stripe.com/00gdQW4NFak43tueUU","_blank");
+});
+
+threeYMember.addEventListener("click", function(){
+	window.open("https://buy.stripe.com/28oeV0fsj77S3tu4gk","_blank");
+});
+
+honorMember.addEventListener("click", function(){
+	window.open("https://buy.stripe.com/eVa6ou5RJ77S8NOeUW","_blank");
+});
